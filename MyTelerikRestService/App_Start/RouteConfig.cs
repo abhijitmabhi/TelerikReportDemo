1cff178786b9dc470ba9b2ab3449bd6113a605ab
Isti <mdistiakuddin39@gmail.com>
2020-10-13 23:54:39 +0600

    Save file working on click button

--- MyTelerikRestService/App_Start/RouteConfig.cs
+++ MyTelerikRestService/App_Start/RouteConfig.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
+using System.Web.Mvc;
 //using Microsoft.AspNet.FriendlyUrls;
 
 namespace MyTelerikRestService
@@ -10,6 +11,13 @@ namespace MyTelerikRestService
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ MyTelerikRestService/Controllers/SaveReportController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Telerik.Reporting.Cache.File;
+using Telerik.Reporting.Processing;
+using Telerik.Reporting.Services;
+using Telerik.Reporting.Services.WebApi;
+using System.Web.Mvc;
+
+namespace MyTelerikRestService.Controllers
+{
+    public class SaveReportController: Controller
+    {
+
+        [System.Web.Http.HttpPost]
+        public void SaveReport()
+        {
+            try
+            {
+                SmartCampusDBEntities db = new SmartCampusDBEntities();
+                var list = db.Admission_Applicant_LeadSource.ToList();
+                Telerik.Reporting.Report report = new Telerik.Reporting.Report();
+                report.DataSource = list;
+                //report.DocumentName = "file.pdf";
+                report.Visible = true;
+                string fileName = HttpContext.Server.MapPath("~/");
+                fileName = Path.Combine(fileName, "Uploads");
+                fileName = Path.Combine(fileName, "file.pdf");
+                //fileName = Path.Combine(fileName, "file.pdf");
+                ReportProcessor reportProcessor = new ReportProcessor();
+                Telerik.Reporting.InstanceReportSource instanceReportSource = new Telerik.Reporting.InstanceReportSource();
+                instanceReportSource.ReportDocument = report;
+                RenderingResult result = reportProcessor.RenderReport("PDF", instanceReportSource, null);
+
+                using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                {
+                    fs.Write(result.DocumentBytes, 0, result.DocumentBytes.Length);
+                }
+            }
+            catch(Exception ex)
+            {
+                string error = ex.Message;
+            }
+        }
+    }
+}
